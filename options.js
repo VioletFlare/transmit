68ccab7f0a990514f7cd8c6fb130b26459c9757d
VioletFlare <violetflare@protonmail.com>
2020-08-22 23:52:07 +0200

    change custom options; update version in manifest

--- options.js
+++ options.js
@@ -1,18 +1,29 @@
 class Options {
 
     constructor() {
-        this.$RPCURL = document.querySelector('#RPCURL');
+        this.$IP = document.querySelector('#ip');
+        this.$port = document.querySelector('#port');
         this.$primaryDownloadDir = document.querySelector('#primaryDownloadDir');
         this.$secondaryDownloadDir = document.querySelector('#secondaryDownloadDir');
         this.$saveButton = document.querySelector('button');
+        this.$protocol = document.querySelector('#protocol');
+    }
+
+    _setProtocol(selectedProtocol) {
+        const $selectedOption = this.$protocol.querySelector(`[value=${selectedProtocol}]`);
+        $selectedOption.setAttribute('selected', true);
     }
 
     _fillForm(options) {
-        const isRPCURLDefined = options.RPCURL !== 'undefined',
+        const isProtocolDefined = options.protocol !== 'undefined',
+            isIPDefined = options.IP !== 'undefined',
+            isPortDefined = options.port !== 'undefined',
             isPrimaryDownloadDirDefined = options.primaryDownloadDir !== 'undefined',
             isSecondaryDownloadDirDefined = options.secondaryDownloadDir !== 'undefined';
 
-        if (isRPCURLDefined) this.$RPCURL.value = options.RPCURL;
+        if (isProtocolDefined) this._setProtocol(options.protocol);
+        if (isIPDefined) this.$IP.value = options.IP;
+        if (isPortDefined) this.$port.value = options.port;
         if (isPrimaryDownloadDirDefined) this.$primaryDownloadDir.value = options.primaryDownloadDir;
         if (isSecondaryDownloadDirDefined) this.$secondaryDownloadDir.value = options.secondaryDownloadDir;
     }
@@ -25,9 +36,21 @@ class Options {
         );
     }
 
+    _getSelectedProtocol() {
+        const $protocol = document.querySelector('#protocol'),
+            selectedProtocol = $protocol.selectedOptions[0].value;
+            
+        return selectedProtocol;
+    }
+
     _saveOptions() {
-        const options = {
-            RPCURL: this.$RPCURL.value,
+        const selectedProtocol = this._getSelectedProtocol(),
+        RPCURL = `${selectedProtocol}://${this.$IP.value}:${this.$port.value}/transmission/rpc`,
+        options = {
+            protocol: selectedProtocol,
+            IP: this.$IP.value,
+            port: this.$port.value,
+            RPCURL: RPCURL,
             primaryDownloadDir: this.$primaryDownloadDir.value,
             secondaryDownloadDir: this.$secondaryDownloadDir.value
         }
